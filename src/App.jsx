7e68cfc3b9c8d2089ed761cfb3be30aa21630351
junior mojica <juniormojica26@gmail.com>
2024-08-29 15:43:23 -0500

    adding boostrap import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
 import Landing from './components/Landing/Landing.jsx'
 import SignIn from './components/SignIn/SignIn.jsx';
