848db0ef225fc28ad37dc11fc7059c3013cfd285
junior mojica <juniormojica26@gmail.com>
2024-08-29 15:44:17 -0500

    adding SideNavigation component and routes

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,13 +1,20 @@
 import BarberTable from "../BarberTable/BarberTable"
+import SideNavigation from "../SideNavigation/SideNavigation"
 import s from './DashBoard.module.css'
 export default function DashBoard() {
   return (
     <section className="container">
-      <h1>DashBoard</h1>
+ 
       <section className={s.registerMainContainer}>
+        <div className={`${s.sidebarMainContainer}  shadow p-3 mb-5 bg-body-tertiary rounded`}>
+        <SideNavigation/>
+        </div>
+        <div className={s.barberTableContainer}>
         <BarberTable barberName ='Adrian'/>
         <BarberTable barberName ='Santigo'/>
         <BarberTable barberName ='Miguel'/>
+        </div>
+       
 
       </section>
  
