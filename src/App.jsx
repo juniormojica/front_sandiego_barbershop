f90025759a814024a2e2a2749ecf796051334fc1
junior mojica <juniormojica26@gmail.com>
2024-12-19 12:12:40 -0500

    deleting sidenavigation from dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Landing from './components/Landing/Landing.jsx'
--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -7,9 +7,6 @@ export default function DashBoard () {
 
       <section>
         <section className={`${s.registerMainContainer}`}>
-          {/* <div className={`shadow p-3 bg-body-tertiary rounded ${s.sidebarMainContainer}`}>
-            <SideNavigation />
-          </div> */}
           <div className={s.barberTableContainer}>
             <BarberTable barberName='Junior' />
             <BarberTable barberName='Santiago' />
