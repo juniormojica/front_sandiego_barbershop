3e5efad24b106de02a5b1cb82ff223dba829f7de
junior mojica <juniormojica26@gmail.com>
2024-12-19 12:14:38 -0500

    deleting import from client component

--- src/components/Clients/Client.jsx
+++ src/components/Clients/Client.jsx
@@ -1,5 +1,4 @@
 import s from './Client.module.css'
-import SideNavigation from '../SideNavigation/SideNavigation'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 
